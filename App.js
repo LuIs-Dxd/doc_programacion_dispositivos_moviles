06485d3cf10d30ae4175fff125b08d95b2fc1f5d
LuIs-Dxd <luissonicgamer@gmail.com>
2023-06-07 18:16:57 -0400

    agregar py code

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World ---> Luis Caba 20210799</Text>
       <StatusBar style="auto" />
     </View>
   );
